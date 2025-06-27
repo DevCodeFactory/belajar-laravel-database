9317606ec618f82ac9d46c9bfbeb90a137a01405
azdyf <azdy.fm@google.com>
2025-06-27 20:59:31 +0700

    Query Builder Delete

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -182,4 +182,13 @@ class QueryBuilderTest extends TestCase
         });
     }
 
+    public function testDelete()
+    {
+        $this->insertCategories();
+
+        DB::table('categories')->where('id', '=', 'SMARTPHONE')->delete();
+        $collection = DB::table('categories')->where('id', '=', 'SMARTPHONE')->get();
+        self::assertCount(0, $collection);
+    }
+
 }
