7bff47d38b2322cf9ed85cc961cb03886a71fca1
azdyf <azdy.fm@google.com>
2025-06-27 21:09:09 +0700

    Query Builder Join

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -191,4 +191,37 @@ class QueryBuilderTest extends TestCase
         self::assertCount(0, $collection);
     }
 
+    public function insertProducts()
+    {
+        $this->insertCategories();
+
+        DB::table('products')->insert([
+            'id' => '1',
+            'name' => 'iPhone 16 Pro Max',
+            'category_id' => 'SMARTPHONE',
+            'price' => 22000000
+        ]);
+        DB::table('products')->insert([
+            'id' => '2',
+            'name' => 'Samsung Galaxy S23 Ultra',
+            'category_id' => 'SMARTPHONE',
+            'price' => 19000000
+        ]);
+    }
+
+    public function testJoin()
+    {
+        $this->insertProducts();
+
+        $collection = DB::table('products')
+            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->select('products.id', 'products.name', 'products.price', 'categories.name as category_name')
+            ->get();
+
+        self::assertCount(2, $collection);
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
 }
